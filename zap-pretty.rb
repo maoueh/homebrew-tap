87fa15b59903d60542ebb4151e8c38335216aab6
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-01 23:08:21 -0500

    Brew formula update for zap-pretty version v0.1.1

--- zap-pretty.rb
+++ zap-pretty.rb
@@ -1,9 +1,9 @@
 class ZapPretty < Formula
   desc "A tiny binary to pretty-print Zap production JSON lines"
   homepage "https://github.com/maoueh/zap-pretty"
-  url "https://github.com/maoueh/zap-pretty/releases/download/v0.1.0/zap-pretty_0.1.0_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "a437b5648a9cfd7afa068dd8aa0388c386b1bf0adcfff56756932176a5ccd895"
+  url "https://github.com/maoueh/zap-pretty/releases/download/v0.1.1/zap-pretty_0.1.1_MacOS_x86_64.tar.gz"
+  version "0.1.1"
+  sha256 "a35e2ba3d516d816781f07e5f72e5b1484f54c9d48b5f225d0c86138d806d011"
 
   def install
     bin.install "zap-pretty"
